ce014e41fddbafb771b29e139e8b551e2b093001
RStanimirov <rumen.stanimirov@gmail.com>
2023-12-03 13:06:22 +0200

    Environment variables file added to project folder

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,13 +11,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-# import environ
+import environ
 
 # Initialise environment variables
-# env = environ.Env()
+env = environ.Env()
 
 # Read .env file
-# environ.Env.read_env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0123456789'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
